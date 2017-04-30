788ad6057ccbc5b7527ea1c4c8dfb8d8c68cf725
Vasek-gh <vasilisk_88@mail.ru>
2017-05-01 00:55:48 +0300

    add main

---
+++ main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL, "Russian");
+
+    cout << "start" << endl;
+
+    try
+    {
+#ifdef DEBUG
+    cout << "DEBUG" << endl;
+#endif
+
+#ifdef RELEASE
+    cout << "RELEASE" << endl;
+#endif
+
+#ifdef AM32
+    cout << "32" << endl;
+#endif
+
+#ifdef AM64
+    cout << "64" << endl;
+#endif
+    }
+    catch (...) {
+        std::cerr << "exception" << std::endl;
+    }
+
+    cout << "done" << endl;
+    int i;
+    cin >> i;
+    return 0;
+}
