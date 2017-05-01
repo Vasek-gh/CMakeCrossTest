ac1c02dc92370fdacbebf635bde75a343c083fa6
Vasek-gh <vasilisk_88@mail.ru>
2017-05-01 23:03:02 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,37 +1,64 @@
 ﻿#include <iostream>
+#include <signal.h>
+#include <stdexcept>
 
 using namespace std;
+
+void signalHandler(int sig)
+{
+    throw runtime_error("jh");
+}
+
+void test()
+{
+    int* i = nullptr;
+    *i = 1;
+}
 
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "Russian");
+    signal(SIGSEGV, signalHandler);
 
     cout << "start" << endl;
 
     try
     {
 #ifdef DEBUG
-    cout << "DEBUG" << endl;
+        cout << "DEBUG" << endl;
 #endif
 
 #ifdef RELEASE
-    cout << "RELEASE" << endl;
+        cout << "RELEASE" << endl;
 #endif
 
 #ifdef AM32
-    cout << "32" << endl;
+        cout << "32" << endl;
 #endif
 
 #ifdef AM64
-    cout << "64" << endl;
+        cout << "64" << endl;
 #endif
+
+#ifdef WIN
+        cout << "Windows" << endl;
+#endif
+
+#ifdef UNIX
+        cout << "Unix" << endl;
+#endif
+
+#ifdef MINGW
+        cout << "MinGw" << endl;
+#endif
+
+
+        test();
     }
     catch (...) {
         std::cerr << "exception" << std::endl;
     }
 
-    cout << "done" << endl;
-    int i;
-    cin >> i;
+    cout << "done" << endl;    
     return 0;
 }
